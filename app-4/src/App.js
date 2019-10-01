78780135b59884214180845164d58f8f2998466a
RobnettSean22 <robnettsean22@gmail.com>
2019-10-01 16:50:21 -0700

    first commit

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -3,16 +3,38 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      username:'',
+      password:''
+    }
+    this.but = this.but.bind(this)
+  }
+  userName(input){
+    this.setState({
+      username:input
+    })
+  }
+  passWord(input1){
+    this.setState({
+      password:input1
+    })
+  }
+
+  but(){
+    alert(`Username:${this.state.username} Password:${this.state.password} `)
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <input onChange={(e)=>{this.userName(e.target.value)}}/>
+      
+       <input onChange={(e)=>{this.passWord(e.target.value)}}/>
+       
+       <button onClick={this.but}>login</button>
       </div>
     );
   }
