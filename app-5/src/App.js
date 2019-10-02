2a4798a1f1038184bd79312854f369e0b45b522b
RobnettSean22 <robnettsean22@gmail.com>
2019-10-02 00:39:57 -0700

    fifth commit

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from './component/image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Image url={"https://vignette.wikia.nocookie.net/tier-zoo/images/d/d2/Lion.jpg/revision/latest?cb=20181109231317"}/>
       </div>
     );
   }
