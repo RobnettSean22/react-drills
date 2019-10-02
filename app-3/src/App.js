4a70497d659793eb6e9dc14f1dc439a3d46f88d7
RobnettSean22 <robnettsean22@gmail.com>
2019-10-01 23:39:42 -0700

    third commit

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      search:'',
+      anime:['SAO', 'Future Diary', 'One Piece', 'Black Lagoon']
+    }
+  }
+
+  narrow(filter){
+    this.setState({
+      search:filter
+    })
+  }
   render() {
+    let animeShows = this.state.anime.filter((shows,index)=>{return shows.includes(this.state.search)})
+    .map((shows,index)=>{return <h1 key={index}>{shows}</h1>})
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e)=>{this.narrow(e.target.value)}} />
+        <div>{animeShows}</div>
       </div>
     );
   }
